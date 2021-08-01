e632500a6dd38804affcde9de70e0af3df2abebd
JohnPYar <johnpyar@gmail.com>
2021-08-01 11:12:17 +0300

    Twice questions

--- js/script.js
+++ js/script.js
@@ -15,11 +15,11 @@ const personalMovieDB = {
 const userLastFilm1 = prompt("Один из последних просмотренных фильмов?", "");
 const userFilmGrade1 = prompt("На сколько оцените его?", "5.0");
 
-personalMovieDB["movies"][userLastFilm1] = userFilmGrade1;
+personalMovieDB.movies[userLastFilm1] = userFilmGrade1;
 
 const userLastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
 const userFilmGrade2 = prompt("На сколько оцените его?", "5.0");
 
-personalMovieDB["movies"][userLastFilm2] = userFilmGrade2;
+personalMovieDB.movies[userLastFilm2] = userFilmGrade2;
 
 console.log(personalMovieDB);

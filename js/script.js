4e6ecb80db33a0c044345311b3aace4166c5a048
JohnPYar <johnpyar@gmail.com>
2021-08-01 10:47:02 +0300

    First practice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log(1);
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {
+
+    },
+    actors : {
+
+    },
+    genres : [
+
+    ],
+    privat : false
+};
+const userLastFilm = prompt("Один из последних просмотренных фильмов?", "");
+const userFilmGrade = prompt("На сколько оцените его?", "5.0");
+
+personalMovieDB["movies"][userLastFilm] = userFilmGrade;
+
+console.log(personalMovieDB);
